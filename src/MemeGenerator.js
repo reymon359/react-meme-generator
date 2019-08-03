1f37e0ec19912d2d1abdec7177ac397bfd7fa765
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-08-03 17:47:04 +0200

    fetch memes

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -9,6 +9,15 @@ class MemeGenerator extends Component {
             randomImg: "http://i.imgflip.com/1bij.jpg"
         }
     }
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response => {
+                const {memes} = response.data
+                console.log(memes[0])
+                this.setState({ allMemeImgs: memes })
+            })
+    }
     
     render() {
         return (
