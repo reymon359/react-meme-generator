f61d18d1f4503ea7f3c46cb1602b17d784266722
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-08-03 18:21:24 +0200

    Updates

---
+++ precache-manifest.2a30892977ce1fb553d4135ae546df1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0ed5530a454f2a859792cf5346d9c9",
+    "url": "/react-meme-generator/index.html"
+  },
+  {
+    "revision": "c9d194036420cdff6261",
+    "url": "/react-meme-generator/static/css/main.0ac9ad1c.chunk.css"
+  },
+  {
+    "revision": "0f19e2518a3a17b37f23",
+    "url": "/react-meme-generator/static/js/2.e2adb5f9.chunk.js"
+  },
+  {
+    "revision": "c9d194036420cdff6261",
+    "url": "/react-meme-generator/static/js/main.b5676c04.chunk.js"
+  },
+  {
+    "revision": "9c46cf9ee4bf95027128",
+    "url": "/react-meme-generator/static/js/runtime~main.b08da8a3.js"
+  }
+]);
